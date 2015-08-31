f3afb812c521e9ff7d4061fc9d9847afa43368a7
Karina Awoki <k.sueminha@hotmail.com>
2015-08-31 14:19:07 -0300

    shell

---
+++ compare_jansen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+max=10
+metade=$(($1/2))
+
+for seed in `seq 1 $max`
+do
+    #./_gen $1 $seed   > comp/t$1s$seed
+    echo "num_vertices: $1   SEED: $seed   metade:$metade"
+    time ./jansen comp/t$1s$seed $metade
+    echo -e
+    echo -e
+done
